7208e9bcf171824fa3961476adc122152f081764
Yakov Keselman <yakov.keselman@gmail.com>
2019-01-10 21:54:45 -0800

    Remove old test

--- test/scrape_person.py
+++
@@ -1,4 +0,0 @@
-from linkedin_user_scraper import scraper
-
-rick_fox = scraper.Person("https://www.linkedin.com/in/rifox?trk=pub-pbmap")
-iggy = scraper.Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5") 
